7ad2f7d0bee3013723c236e0c1c2d2f18ac0a653
kurt miller <kmiller9393@gmail.com>
2019-02-20 23:27:57 -0700

    Update App.js, add icons

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './components/Login/Login';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Login />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%'
+  }
+});
