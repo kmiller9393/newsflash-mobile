fd0d6e56ce71d91f3af654314fdda8a6671551bb
kurt miller <kmiller9393@gmail.com>
2019-02-22 20:15:14 -0700

    Add Home component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Login from './components/Login/Login';
 
 export default class App extends Component {
