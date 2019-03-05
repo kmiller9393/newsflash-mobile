5c10329c081b80ef5923a75f9541b775722860af
kurt miller <kmiller9393@gmail.com>
2019-03-04 18:28:01 -0700

    Remove View component, add withApollo so App has proper context

--- App.js
+++ App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import { ApolloClient } from 'apollo-boost';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloProvider } from 'react-apollo';
-import { StyleSheet, View } from 'react-native';
+import { ApolloProvider, withApollo } from 'react-apollo';
 import { createAppContainer, createDrawerNavigator } from 'react-navigation';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Profile from './components/Profile/Profile';
 import Home from './components/Home/Home';
 
+
 export default class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       loggedIn: false
     };
@@ -25,9 +25,15 @@ export default class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <View style={styles.container}>
-          {this.state.loggedIn ? <AuthAppContainer /> : <NoAuthAppContainer />}
-        </View>
+        {this.state.loggedIn ? (
+          <FinalAuthAppContainer
+            screenProps={{ changeLoginState: this.handleChangeLoginState }}
+          />
+        ) : (
+            <FinalNoAuthAppContainer
+              screenProps={{ changeLoginState: this.handleChangeLoginState }}
+            />
+          )}
       </ApolloProvider>
     );
   }
@@ -50,13 +56,6 @@ const AuthDrawerNavigator = createDrawerNavigator({
   Home: { screen: Home }
 });
 
-const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-    width: '100%'
-  }
-});
-
 const client = new ApolloClient({
   link: new HttpLink({ uri: 'https://newsflashback.herokuapp.com/graphql/' }),
   cache: new InMemoryCache()
@@ -65,3 +64,7 @@ const client = new ApolloClient({
 const NoAuthAppContainer = createAppContainer(NoAuthDrawerNavigator);
 
 const AuthAppContainer = createAppContainer(AuthDrawerNavigator);
+
+const FinalNoAuthAppContainer = withApollo(NoAuthAppContainer);
+
+const FinalAuthAppContainer = withApollo(AuthAppContainer);
