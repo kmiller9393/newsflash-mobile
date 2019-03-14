184605734e98091bcf45ce82b547016e5f08c411
kurt miller <kmiller9393@gmail.com>
2019-03-13 21:11:10 -0600

    Refactor StoryContainer StyleSheet, move to separate file

---
+++ src/components/StoryContainer/StoryContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, ScrollView } from 'react-native';
+import styles from './styles';
+import StoryCard from '../StoryCard/StoryCard';
+import stories from '../../../utils/mockStories.json';
+
+const storyCards = stories.map(story => (
+  <StoryCard {...story} key={story.id} />
+));
+
+const StoryContainer = () => {
+  return (
+    <View style={styles.storyContainer}>
+      <ScrollView>
+        <View>{storyCards}</View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default StoryContainer;
